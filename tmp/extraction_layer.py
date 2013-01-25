95775c9ecc189c5d3b0d6fdb2021ba8de0f01fac
tonyro <tony.rovba@gmaril.com>
2013-01-25 18:46:10 +0200

    inserting finally works

--- tmp/extraction_layer.py
+++ tmp/extraction_layer.py
@@ -3,7 +3,7 @@ import os
 
 file_path = '/home/nurka/Stat/Costs/'
 #file_path = '/home/tonyr/Work/Costs/Data/costs_20121207.csv'
-DB_QUERY_GET_LIST_OF_FILES = "select file_name from costs.files_arrivals_control"
+DB_QUERY_GET_LIST_OF_FILES = "select file_name from costs.files_arrivals_control;"
 db_name = "costs"
 db_user = "root"
 db_pwd = "root"
@@ -20,22 +20,26 @@ cur = db_conn.cursor()
 rows_affected = cur.execute(DB_QUERY_GET_LIST_OF_FILES)
 print(rows_affected)
 if rows_affected == 0:
-    print('There is no files in DB.')
+    print 'There is no files in DB.'
 else:
     db_result = cur.fetchall() # returns 2-dim array
     for nme in db_result[0]:
         db_list_of_files.append(nme)
-print(db_list_of_files)
+print db_list_of_files
 
 # Get list of file names from filesystem
 for file_name in os.listdir(file_path):
     folder_list_of_files.append(file_name)
-print(folder_list_of_files)
+print folder_list_of_files
 
 # Get file names that present in filesystem, but do not present in DB
 files_to_process = [x for x in folder_list_of_files if x not in db_list_of_files]
-print(files_to_process)
+print "Files to process are:", files_to_process
 
+for nme in files_to_process:
+    rows_affected = cur.execute("insert into costs.files_arrivals_control (file_name, day, arrival_time, processed_time, processed_flag) values ('%s', '20121006', null, null, 'N'); commit;" % (nme))
+    print "Rows when insert", rows_affected
+    if rows_affected == 0:
+        print "Record for file", nme, "was not inserted to FILES_ARRIVALS_CONTROL."
 
-
-db_conn.close()
+#db_conn.close()
